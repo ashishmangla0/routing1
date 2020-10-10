ea1178d2f2b2f72a14413f740fadaeb7167bda75
ashish <ashishmangla1991@gmail.com>
2020-10-10 21:42:21 +0530

    add router to  app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Routes from "./routes";
 import Sidebar from "./components/Sidebar/Sidebar";
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <>
       <div className="dashboard">
-        <Sidebar />
-        <Routes />
+        <Router>
+          <Sidebar />
+          <Routes />
+        </Router>
       </div>
     </>
   );
